6eda75d005e76a7c6c90fe899848d512530e508c
Fred Nyaga <fredjamo68@gmail.com>
2020-10-21 20:25:26 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    document.title = 'facebooki'
+  })
+
+  return (
+    <div className="App">
+      yo!
+      
+    </div>
+  );
+}
+
+export default App;
